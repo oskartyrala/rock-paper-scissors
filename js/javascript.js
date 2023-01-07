2b9935a99c3feb332491b75c75f92ab938820558
oskartyrala <oskartyrala@protonmail.com>
2023-01-07 22:24:17 +0100

    Plan the program with comments

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+// Play a best-of-5 game of rock, paper, scissors;
+// For each round of the game:
+// Generate computer's choice of rock, paper, scissors;
+// Store computer's choice in a variable;
+// Ask the player's choice of rock, paper, scissors;
+// Store player's choice in a variable;
+// Compare the player's choice to the computer's choice;
+// Log the result and the winner;
+// Update the total score;
+// Log the total score
+// If one of the competitors reaches 3 points, end the game and declare the winner;
